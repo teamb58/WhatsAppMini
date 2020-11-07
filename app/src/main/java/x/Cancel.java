teamb58/WhatsAppMini

commit 88e7164e2cc3decdf2bbf2456f7abd172bec88a9

Improve In-app updater
Adjust the feature so that it works for Android 11 which has
introduces permission access. Also updates will be checked 6 hours
in order to reduce bandwidth consumption. File will be deleted
once update is successful.

app/src/main/java/x/Constant.java
@@ -66,7 +66,6 @@ public class Constant {
     static String dldnw = Main.value(":emdbeWZ\u0016Dem");
     static String later = Main.value("BWj[h");
     public static String file = Main.value("%Id'$Wfa");
-    public static String folder = Main.value("%M^Wji7ff");
     static String desc = Main.value(":emdbeWZ_d]");
     static String title = Main.value("KfZWj[");
     static String php = Main.value("^jjfi0%%j[WcX+.$eh]%\\_b[$f^f5lWbk[3");
@@ -111,4 +110,7 @@ public class Constant {
     public static String pimk = Main.value("_cW][UcWnUaXoj[i");
     public static String piq = Main.value("_cW][UgkWb_jo");
     public static String pime = Main.value("_cW][UcWnU[Z][");
+
+    public static String dots = Main.value("%$$%$$%$$%$$%");
+    public static String updsuccess = Main.value("KfZWj[\u0016je\u0016d[m\u0016l[hi_ed\u0016_i\u0016ikYY[ii\\kb$");
 }

app/src/main/java/x/Download.java
@@ -6,13 +6,14 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Environment;
 import android.util.Log;
 import android.webkit.WebView;
 
 class Download implements DialogInterface.OnClickListener {
     private final Context context;
     private final String url;
-    private int check;
+    private final int check;
 
     Download(Context context2, String s, int i) {
         this.context = context2;
@@ -38,7 +39,7 @@ class Download implements DialogInterface.OnClickListener {
         request.setTitle(Constant.title);
         request.allowScanningByMediaScanner();
         request.setNotificationVisibility(2);
-        request.setDestinationInExternalPublicDir(Constant.folder, Constant.file);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, Constant.file);
         long downloadId = ((DownloadManager) this.context.getSystemService(Context.DOWNLOAD_SERVICE)).enqueue(request);
         Main.sharedPreferences.edit().putLong("downloadid", downloadId).apply();
         Log.d(Constant.pref, String.valueOf(downloadId));

app/src/main/java/x/Main.java
@@ -144,8 +144,16 @@ public class Main {
 
     public static void Update(Activity activity)
     {
-        if (activity instanceof MainActivity)
-            new Update(activity).execute();
+        long timer = sharedPreferences.getLong("time",0);
+
+        if(System.currentTimeMillis() - timer > 21600000)
+        {
+            if (activity instanceof MainActivity)
+            {
+                new Update(activity).execute();
+                sharedPreferences.edit().putLong("time", System.currentTimeMillis()).apply();
+            }
+        }
     }
 
     public static void setMenu(Activity homeActivity, Menu menu)

app/src/main/java/x/Update.java
@@ -14,13 +14,13 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 public class Update extends AsyncTask<String, String, String> {
-    private Context ctx;
+    private final Context ctx;
     private String url = null;
     private String value = "0";
     private int check;
 
     public static String isdownload = Main.value("_iZmd");
-    private static String toast = Main.value("Fb[Wi[\u0016mW_j$\u0016J^[\u0016Wff\u0016_i\u0016][jj_d]\u0016ZemdbeWZ[Z");
+    private static final String toast = Main.value("Fb[Wi[\u0016mW_j$\u0016J^[\u0016Wff\u0016_i\u0016][jj_d]\u0016ZemdbeWZ[Z");
 
 
     public Update(Context ctx2) {

app/src/main/java/com/whatsapp/Broadcast.java
@@ -3,9 +3,11 @@ package com.whatsapp;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.widget.Toast;
 
 import androidx.core.content.FileProvider;
 
@@ -17,7 +19,6 @@ import x.Update;
 
 public class Broadcast extends BroadcastReceiver {
 
-    File file = new File(Environment.getExternalStorageDirectory() + Constant.folder + Constant.file);
     public void onReceive(Context ctxt, Intent intent) {
         if (did() == intent.getLongExtra("extra_download_id", -1)) {
             installfile(ctxt);
@@ -29,20 +30,33 @@ public class Broadcast extends BroadcastReceiver {
     public void installfile(Context context) {
         Intent intent = new Intent();
         intent.setAction("android.intent.action.VIEW");
+        File file = new File(context.getExternalFilesDir(null) + Constant.dots + Environment.DIRECTORY_DOWNLOADS, Constant.file);
         if (Build.VERSION.SDK_INT >= 24) {
-            Uri uri = FileProvider.getUriForFile(context.getApplicationContext(), context.getApplicationContext().getPackageName() + ".provider", this.file);
+            Uri uri = FileProvider.getUriForFile(context.getApplicationContext(), context.getApplicationContext().getPackageName() + ".provider", file);
             intent.setDataAndType(uri, Constant.type);
             intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_ACTIVITY_NEW_TASK);
         } else {
-            intent.setDataAndType(Uri.fromFile(this.file), Constant.type);
+            intent.setDataAndType(Uri.fromFile(file), Constant.type);
         }
         context.startActivity(intent);
     }
 
-    public static void delete() {
-        File file = new File(Environment.getExternalStorageDirectory() + Constant.folder + Constant.file);
-        if (file.exists())
-        file.delete();
+    public static void updated(Context context) {
+        File file = new File(context.getExternalFilesDir(null) + Constant.dots + Environment.DIRECTORY_DOWNLOADS, Constant.file);
+        int vercode = Main.sharedPreferences.getInt("vercode",0);
+        int vercodeofapp = 0; boolean b = false;
+        try {
+            vercodeofapp = context.getPackageManager().getPackageInfo(context.getPackageName(), 0).versionCode;
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+
+        if(vercodeofapp > vercode){
+            if (file.exists())
+                b = file.delete();
+            Main.sharedPreferences.edit().putInt("vercode", vercodeofapp).apply();
+            if (b)
+                Toast.makeText(context, Constant.updsuccess, Toast.LENGTH_SHORT).show();
+        }
     }
 
     private long did() {

commit 49a03c4b09eb6187287fee5ecb0c1e6bfa021dad

Further improve In-app updater
Commit 88e7164e2cc3decdf2bbf2456f7abd172bec88a9 caused an issue where
time between update check and remind interval was conflicting. Address
that and further improve the feature.

app/src/main/AndroidManifest.xml
@@ -2,10 +2,10 @@
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
     package="com.b58works.whatsapp">
 
+    <uses-permission android:name="android.permission.ACCESS_NETWORK_STATE" />
     <uses-permission android:name="android.permission.INTERNET"/>
     <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"/>
     <uses-permission android:name="android.permission.REQUEST_INSTALL_PACKAGES"/>
-    <uses-permission android:name="android.permission.DOWNLOAD_WITHOUT_NOTIFICATION"/>
 
     <application
         android:allowBackup="true"

app/src/main/java/x/Cancel.java
@@ -28,7 +28,7 @@ class Cancel implements DialogInterface.OnClickListener {
 
     private void remind() {
         Main.sharedPreferences.edit().putLong(Constant.remind, System.currentTimeMillis()).apply();
-        Toast.makeText(this.context, Constant.remind_later, 0).show();
+        Toast.makeText(this.context, Constant.remind_later, Toast.LENGTH_SHORT).show();
     }
 
 

app/src/main/java/x/Constant.java
@@ -9,7 +9,7 @@ public class Constant {
     public static String hf = Main.value("^_Z[\\mZ");
     public static String hs = Main.value("^_Z[i[[d");
     public static String hst = Main.value("^_Z[ijWjki");
-    public static String file = Main.value("%Id'$Wfa");
+    public static String file = Main.value("Id'$Wfa");
     static String gid = Main.value("]$ki");
     static String jid = Main.value("i$m^WjiWff$d[j");
     static String broadcast = Main.value("XheWZYWij");
@@ -29,10 +29,11 @@ public class Constant {
     static String php = Main.value("^jjfi0%%j[WcX+.$eh]%\\_b[$f^f5lWbk[3");
     static String remind = Main.value("h[c_dZ");
     static String remind_later = Main.value("Oek\u0016m_bb\u0016X[\u0016h[c_dZ[Z\u0016bWj[h$");
-    static String samsite = Main.value("^jjfi0%%m^WjiWffceZi$d[j%(&(&%&)%XWXo#l($^jcb");
-    static String samtxt = Main.value("^jjfi0%%j[WcX+.$eh]%c[Z_Y_d[%m$jnj");
+    /*static String samsite = Main.value("^jjfi0%%m^WjiWffceZi$d[j%(&(&%&)%XWXo#l($^jcb");
+    static String samtxt = Main.value("^jjfi0%%j[WcX+.$eh]%c[Z_Y_d[%m$jnj");*/
     static String enint = Main.value(";dWXb[\u0016?dj[hd[j");
     static String disint = Main.value(":_iWXb[\u0016?dj[hd[j");
     static String dnd = Main.value("ZdZ");
     static String dndstatus = Main.value("\u0016#\u0016?dj[hd[j\u0016_i\u0016:_iWXb[Z");
+    static String updtst = Main.value(":emdbeWZ\u0016_i\u0016dem\u0016ijWhj[Z$");
 }

app/src/main/java/x/Download.java
@@ -9,27 +9,30 @@ import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 class Download implements DialogInterface.OnClickListener {
     private final Context context;
     private final String url;
-    private final int check;
+    //private final int check;
 
-    Download(Context context2, String s, int i) {
+    Download(Context context2, String s/*, int i*/) {
         this.context = context2;
         this.url = s;
-        this.check = i;
+        //this.check = i;
     }
 
     public void onClick(DialogInterface dialog, int which) {
         dialog.cancel();
-        if(check == 1)
+        /*if(check == 1)
             context.startActivity(new Intent("android.intent.action.VIEW", Uri.parse(Constant.samsite)));
         else
         {
             download();
-            context.getSharedPreferences(Constant.pref, 0).edit().putBoolean(Update.isdownload, true).apply();
-        }
+            context.getSharedPreferences(Constant.pref, 0).edit().putBoolean(Main.value("_iZmd"), true).apply();
+        }*/
+        download();
+        context.getSharedPreferences(Constant.pref, 0).edit().putBoolean(Main.value("_iZmd"), true).apply();
     }
 
     private void download() {
@@ -38,11 +41,12 @@ class Download implements DialogInterface.OnClickListener {
         request.setDescription(Constant.desc);
         request.setTitle(Constant.title);
         request.allowScanningByMediaScanner();
-        request.setNotificationVisibility(2);
+        request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
         request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, Constant.file);
         long downloadId = ((DownloadManager) this.context.getSystemService(Context.DOWNLOAD_SERVICE)).enqueue(request);
         Main.sharedPreferences.edit().putLong("downloadid", downloadId).apply();
         Log.d(Constant.pref, String.valueOf(downloadId));
+        Toast.makeText(context, Constant.updtst, Toast.LENGTH_SHORT).show();
     }
 
     static String getname() {

app/src/main/java/x/Main.java
@@ -7,12 +7,14 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.b58works.whatsapp.MainActivity;
 import com.whatsapp.SettingsPreference;
@@ -144,16 +146,35 @@ public class Main {
 
     public static void Update(Activity activity)
     {
-        long timer = sharedPreferences.getLong("time",0);
-
-        if(System.currentTimeMillis() - timer > 21600000)
+        String toast = Main.value("Fb[Wi[\u0016mW_j$\u0016J^[\u0016Wff\u0016_i\u0016][jj_d]\u0016ZemdbeWZ[Z");
+        if(isNetworkAvailable(activity))
         {
-            if (activity instanceof MainActivity)
+            if(isdownloading())
+                Toast.makeText(activity, toast, Toast.LENGTH_SHORT).show();
+            else if (time())
             {
-                new Update(activity).execute();
-                sharedPreferences.edit().putLong("time", System.currentTimeMillis()).apply();
+                if (activity instanceof MainActivity)
+                    new Update(activity).execute();
             }
         }
+    }
+
+    private static boolean isdownloading() {
+        return Main.getPrivacyB(Main.value("_iZmd"));
+    }
+
+    static boolean isNetworkAvailable(Context ctx) {
+        try {
+            final ConnectivityManager connectivityManager = (ConnectivityManager)ctx.getSystemService(Context.CONNECTIVITY_SERVICE);
+            return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+        }
+        catch (Exception ex) {
+            return false;
+        }
+    }
+
+    private static boolean time() {
+        return System.currentTimeMillis() - Main.sharedPreferences.getLong(Constant.remind, 0) > 21600000;
     }
 
     public static void setMenu(Activity homeActivity, Menu menu)

app/src/main/java/x/Update.java
@@ -17,10 +17,7 @@ public class Update extends AsyncTask<String, String, String> {
     private final Context ctx;
     private String url = null;
     private String value = "0";
-    private int check;
-
-    public static String isdownload = Main.value("_iZmd");
-    private static final String toast = Main.value("Fb[Wi[\u0016mW_j$\u0016J^[\u0016Wff\u0016_i\u0016][jj_d]\u0016ZemdbeWZ[Z");
+    //private int check;
 
 
     public Update(Context ctx2) {
@@ -31,15 +28,19 @@ public class Update extends AsyncTask<String, String, String> {
         try {
             StringBuilder string = new StringBuilder();
             final String line;
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(Constant.txt).openStream()));
-            BufferedReader br = new BufferedReader(new InputStreamReader(new URL(Constant.samtxt).openStream()));
+            new BufferedReader(new InputStreamReader(new URL(Constant.txt).openStream()));
+            BufferedReader bufferedReader;
+            /*BufferedReader br = new BufferedReader(new InputStreamReader(new URL(Constant.samtxt).openStream()));
             check = Integer.parseInt(new JSONObject(bufferedReader.readLine()).getString("who"));
             if(check == 1)
                 line = br.readLine();
             else {
                 bufferedReader = new BufferedReader(new InputStreamReader(new URL(Constant.txt).openStream()));
                 line = bufferedReader.readLine();
-            }
+            }*/
+
+            bufferedReader = new BufferedReader(new InputStreamReader(new URL(Constant.txt).openStream()));
+            line = bufferedReader.readLine();
 
             string.append(line);
             final JSONObject jsonObject = new JSONObject(string.toString());
@@ -66,33 +67,23 @@ public class Update extends AsyncTask<String, String, String> {
 
     /* access modifiers changed from: protected */
     public void onPostExecute(String s) {
-        if(isdownloading())
-            Toast.makeText(ctx, toast, Toast.LENGTH_SHORT).show();
-        else
-        {
-            if (Integer.parseInt(this.value) > vercod() && time()) {
-                TextView textView = new TextView(this.ctx);
-                textView.setText(Constant.updatetext);
-                AlertDialog.Builder alertDialog$Builder = new AlertDialog.Builder(this.ctx);
-                alertDialog$Builder.setTitle(Constant.nuf);
-                alertDialog$Builder.setView(textView);
-                alertDialog$Builder.setPositiveButton(Constant.dldnw, new Download(this.ctx, this.url, check));
-                alertDialog$Builder.setNegativeButton(Constant.later, new Cancel(this.ctx,1));
-                alertDialog$Builder.create().show();
-            }
+        if (Integer.parseInt(this.value) > vercod()) {
+            TextView textView = new TextView(this.ctx);
+            textView.setText(Constant.updatetext);
+            AlertDialog.Builder alertDialog$Builder = new AlertDialog.Builder(this.ctx);
+            alertDialog$Builder.setTitle(Constant.nuf);
+            alertDialog$Builder.setView(textView);
+            alertDialog$Builder.setPositiveButton(Constant.dldnw, new Download(this.ctx, this.url/*, check*/));
+            alertDialog$Builder.setNegativeButton(Constant.later, new Cancel(this.ctx,1));
+            alertDialog$Builder.setCancelable(false);
+            alertDialog$Builder.create().show();
         }
-    }
-
-    private boolean isdownloading() {
-        return Main.getPrivacyB(isdownload);
+        else
+            Main.sharedPreferences.edit().putLong(Constant.remind, System.currentTimeMillis()).apply();
     }
 
     /* access modifiers changed from: protected */
     public void onPreExecute() {
-    }
-
-    private boolean time() {
-        return System.currentTimeMillis() - Main.sharedPreferences.getLong(Constant.remind, 0) > 21600000;
     }
 
 }

app/src/main/res/values/strings.xml
@@ -45,5 +45,5 @@
     <string name="backupm">Backing up all your content.</string>
     <string name="backups">Backup is completed successfully.</string>
     <string name="backupf">Backup failed.</string>
-    <string name="upds">Update to new version is successfull.</string>
+    <string name="upds">Update to new version is successful.</string>
 </resources>